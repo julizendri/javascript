f61e3331b062b65ddd3279dde4ae51914aa13548
Julieta Zendri <juzendri@gmail.com>
2024-05-22 20:16:35 -0300

    cambios a cantidad de tramites y al codigo en gral para que no se pueda ingresar menos de uno

--- js/main.js
+++ js/main.js
@@ -7,74 +7,66 @@ alert("Nuestro trabajo es ayudarle a facilitar el proceso para aplicar a las vis
 
 let cantidadDeTramites = parseInt(prompt("¿Cuántas personas quieren realizar el trámite? (Ingrese la cantidad en números)."));
 
-while (isNaN(cantidadDeTramites)) {
-    cantidadDeTramites = parseInt(prompt("Respuesta inválida: ¿Cuántas personas quieren realizar el trámite? (Ingrese la cantidad en números)."))
+while (isNaN(cantidadDeTramites) || cantidadDeTramites <= 0) {
+    cantidadDeTramites = parseInt(prompt("Respuesta inválida: ¿Cuántas personas quieren realizar el trámite? (Ingrese la cantidad en números)."));
 }
 
 function despedida() {
     alert("Hasta luego!");
 }
 
+const personas = [];
+
 for (let i = 1; i <= cantidadDeTramites; i++) {
-    if (i === 0){
-        despedida();
-        break;
+    let persona = crearPersona(i);
+    personas.push(persona);
+    edad(i);
+    elegirPais(i);
+    console.log(personas)
+}
+
+function crearPersona(i) {
+    const primerNombre = prompt("Ingrese el primer nombre de la persona " + i + PUNTO);
+    const primerApellido = prompt("Ingrese el primer apellido de la persona " + i + PUNTO);
+    const telefono = prompt("Ingrese el teléfono de la persona " + i + PUNTO);
+    const email = prompt("Ingrese el correo electrónico de la persona " + i + PUNTO);
+    return { primerNombre, primerApellido, telefono, email };
+}
+
+function edad(i) {
+    let edad = parseInt(prompt("Ingrese la edad de la persona " + i + " en números."));
+    while (isNaN(edad)) {
+        edad = parseInt(prompt("Respuesta inválida: Ingrese la edad de la persona " + i + " en números."));
     }
-    class Persona {
-        constructor(primerNombre, primerApellido, telefono, email) {
-            this.primerNombre = primerNombre;
-            this.primerApellido = primerApellido;
-            this.telefono = telefono;
-            this.email = email;
-        }
-        recolectaDatos() {
-            console.log("Persona " + i + ":" + this.primerNombre + VACIO + this.primerApellido + VACIO + this.telefono + VACIO + this.email);
-        }
-    }
-    const datos = new Persona(prompt("Ingrese el primer nombre de la persona " + i + PUNTO), prompt("Ingrese el primer apellido de la persona " + i + PUNTO), prompt("Ingrese el número de teléfono de la persona " + i + PUNTO), prompt("Ingrese su dirección de correo electrónico de la persona " + i + PUNTO));
-    datos.recolectaDatos();
-
-    function edad() {
-        let edad = parseInt(prompt("Ingrese la edad de la persona " + i + " en números."));
-        while (isNaN(edad)) {
-            edad = parseInt(prompt("Respuesta inválida: Ingrese la edad de la persona " + i + " en números."))
-        }
-        if (edad >= 18) {
-            alert("La persona " + i + "es mayor de edad. Por favor, siga proporcionando los datos requeridos.");
-        } else if (edad > 0 && edad < 18) {
-            let opcionEdad = prompt("La persona " + i + ", cuenta con un adulto responsable para continuar con el trámite? (si/no).").toLowerCase();
-            if ((opcionEdad === "si") || (opcionEdad === "s") || (opcionEdad === "i")) {
-                alert("Por favor, siga proporcionando los datos requeridos.");
-            } else {
-                alert("Por favor, solicite la ayuda de un adulto responsable de la persona " + i + " para proporcionar los datos requeridos.");
-            }
-        }
-    }
-
-    edad()
-
-
-
-    function elegirPais() {
-        let pais = prompt("¿A qué país desea viajar la persona " + i + "? (si desea seleccionar otro país ingrese otro).").toLowerCase();
-        if ((pais === "estados unidos") || (pais === "estado unidos") || (pais === "estado unido") || (pais === "estados unido") || (pais === "eeuu") || (pais === "eu")) {
-            alert("La persona " + i + " quiere viajar a Estados Unidos.");
-        } else if ((pais === "australia") || (pais === "australi") || (pais === "au")) {
-            alert("La persona " + i + "  quiere viajar a Australia.");
-        } else if ((pais === "canada") || (pais === "canadá") | (pais === "ca")) {
-            alert("La persona " + i + "  quiere viajar a Canadá.")
-        } else if ((pais === "otro") || (pais === "otr")) {
-            alert("Usted ha ingresado otro.");
+    if (edad >= 18) {
+        alert("La persona " + i + " es mayor de edad. Por favor, siga proporcionando los datos requeridos.");
+    } else if (edad > 0 && edad < 18) {
+        let opcionEdad = prompt("La persona " + i + ", cuenta con un adulto responsable para continuar con el trámite? (si/no).").toLowerCase();
+        if ((opcionEdad === "si") || (opcionEdad === "s") || (opcionEdad === "i")) {
+            alert("Por favor, siga proporcionando los datos requeridos.");
         } else {
-            alert("La opción ingresada no está en nuestra base de datos, por favor ingrese Estados Unidos, Australia, Canadá u otro.");
-            let nuevaOpcion = prompt("Desea ingresar una nueva opción? (si o no)").toLowerCase();
-            if ((nuevaOpcion === "si") || (nuevaOpcion === "s") || (nuevaOpcion === "i")) {
-                elegirPais();
-            } else {
-                despedida();
-            }
+            alert("Por favor, solicite la ayuda de un adulto responsable de la persona " + i + " para proporcionar los datos requeridos.");
         }
     }
+}
 
-    elegirPais()
+function elegirPais(i) {
+    let pais = prompt("¿A qué país desea viajar la persona " + i + "? (si desea seleccionar otro país ingrese otro).").toLowerCase();
+    if ((pais === "estados unidos") || (pais === "estado unidos") || (pais === "estado unido") || (pais === "estados unido") || (pais === "eeuu") || (pais === "eu")) {
+        alert("La persona " + i + " quiere viajar a Estados Unidos.");
+    } else if ((pais === "australia") || (pais === "australi") || (pais === "au")) {
+        alert("La persona " + i + "  quiere viajar a Australia.");
+    } else if ((pais === "canada") || (pais === "canadá") | (pais === "ca")) {
+        alert("La persona " + i + " quiere viajar a Canadá.");
+    } else if ((pais === "otro") || (pais === "otr")) {
+        alert("Usted ha ingresado otro.");
+    } else {
+        alert("La opción ingresada no está en nuestra base de datos, por favor ingrese Estados Unidos, Australia, Canadá u otro.");
+        let nuevaOpcion = prompt("Desea ingresar una nueva opción? (si o no)").toLowerCase();
+        if ((nuevaOpcion === "si") || (nuevaOpcion === "s") || (nuevaOpcion === "i")) {
+            elegirPais(i);
+        } else {
+            despedida();
+        }
+    }
 }
